56c5de405419980aee954bdb7699a9d4b1506701
Atishjha <145045485+Atishjha@users.noreply.github.com>
2026-08-13 13:48:24 +0530

    Update test_pg.py

--- backend/test_pg.py
+++ backend/test_pg.py
@@ -1,6 +1,6 @@
 ﻿import asyncio, asyncpg
 async def main():
-    conn = await asyncpg.connect(user="postgres", password="postgres", database="marketplace", host="localhost", port=5433)
+    conn = await asyncpg.connect(user="postgres", password="postgres", database="marketplace", host="localhost", port=55432)
     print(await conn.fetchval("SELECT 1"))
     await conn.close()
 asyncio.run(main())
